6e3dd9443910532d2410b3569c1da4840149daf1
HiddenDev <90830959+Sachin79dev@users.noreply.github.com>
2026-05-21 16:42:30 +0530

    Update app.js
    Formatting.

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ let Num = Number(prompt("Enter a Number"));
 if (Num % 2 == 0) {
     console.log("The Number is Even!");
 }else {
-    console.log("The Number is Odd!");
-    
+    console.log("The Number is Odd!"); 
 }
